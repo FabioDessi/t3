ce9ff90f2bf9277cda0a8e8950702fe9e0d548c2
Fabio Dessi <fabio.dessi@webnos.it>
2020-02-12 06:16:43 +0100

    Basic store setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,46 @@
 import Vue from 'vue';
+import { bestSpot, checkTie, checkWin, emptySquares } from '../utils/'
+const store = Vue.observable({
+  state: {
+    board: Array.from(Array(9).keys()),
+    gameOver: false,
+    gameWon: {},
+    playerTurn: true
+  },
 
-export const store = Vue.observable({
-  board: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  startGame() {
+    this.state.gameOver = false;
+    this.state.board = Array.from(Array(9).keys());
+    this.state.gameWon = Object.assign({})
+  },
+
+  turn(cell, player) {
+    this.state.board.splice(cell, 1, player);
+    if(checkWin(this.state.board, player)) {
+      this.gameOver(checkWin(this.state.board, player));
+    } else {
+      if(checkTie(this.state.board)) this.gameOver({index: null, player: 'Tie Game'})
+    }
+    this.state.playerTurn = false;
+  },
+
+  aiTurn() {
+    if (!this.state.playerTurn && emptySquares(this.state.board).length !== 0 ) {
+      this.state.board.splice(bestSpot(this.state.board), 1, 'X');
+      if(checkWin(this.state.board, 'X')) {
+        this.gameOver(checkWin(this.state.board, 'X'));
+      } else {
+        if(checkTie(this.state.board)) this.gameOver({index: null, player: 'Tie Game'})
+      }
+      this.state.playerTurn = true;
+    }
+  },
+
+  gameOver(gameWon) {
+    this.state.gameOver = true;
+    this.state.gameWon =  Object.assign({}, gameWon);
+  }
+
 });
 
-export const mutations = {
-  turn(cell) {
-    store.board.splice(cell, 1, 'O')
-
-  }
-};
+export default store;
